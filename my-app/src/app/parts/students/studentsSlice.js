e1a5145bd25a2d33b3dc2ba5d72b86d7ef7c2ce8
KethyI <kot1256@gmail.com>
2024-03-23 11:13:47 +0200

    created reducer for the update student

--- my-app/src/app/parts/students/studentsSlice.js
+++ my-app/src/app/parts/students/studentsSlice.js
@@ -24,8 +24,19 @@ const studentsSlice = createSlice({
     studentAdded(state, action) {
       state.push(action.payload);
     },
+    studentUpdated(state, action) {
+      const { id, name, surname, age, speciality } = action.payload;
+      const pickedStudent = state.find((student) => student.id === id);
+
+      if (pickedStudent) {
+        (pickedStudent.name = name),
+          (pickedStudent.surname = surname),
+          (pickedStudent.age = age),
+          (pickedStudent.speciality = speciality);
+      }
+    },
   },
 });
 
-export const { studentAdded } = studentsSlice.actions;
+export const { studentAdded, studentUpdated } = studentsSlice.actions;
 export default studentsSlice.reducer;
